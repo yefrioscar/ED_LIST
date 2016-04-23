ee9b96d5390e0587dc95400caaabd6b54cb36114
Yefri Oscar <yefrioscar9814@hotmail.com>
2016-04-22 19:08:38 -0500

    Agregado el controlador del taskList

--- js/controller.js
+++ js/controller.js
@@ -1,15 +1,18 @@
+'use strict';
+
 var newTaskEl = document.getElementById('new-task');
 var taskList = document.getElementById('task-list');
 var inbox = new List('inbox');
 
-function addTask(e, list = inbox) {
+function addTask(e) {
+  var list = arguments.length <= 1 || arguments[1] === undefined ? inbox : arguments[1];
+
   e.preventDefault();
-  if(e.which === 13) {
+  if (e.which === 13) {
     var newTask = new Task(this.value);
     this.value = "";
     list.addTask(newTask);
     console.table(list.tasks);
-
   }
 }
 
